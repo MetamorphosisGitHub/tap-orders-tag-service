81082b0eaf606dbfba7455a52df33c2c81e6aecf
camarvito <victormacgrath@gmail.com>
2022-01-10 15:51:51 -0300

    refactor: change folder structure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const product = require("./api/product");
+const product = require("./api/subscriptions/infra/http/controllers/product");
 
 app.use(express.json({ extended: false }));
 
