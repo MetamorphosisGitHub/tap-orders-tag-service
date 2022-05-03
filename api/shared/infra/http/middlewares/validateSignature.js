083b48ee807826a755b7473d620fcbcf3a265343
camarvito <victormacgrath@gmail.com>
2022-05-03 14:26:23 -0300

    feat: test middleware

--- api/shared/infra/http/middlewares/validateSignature.js
+++ api/shared/infra/http/middlewares/validateSignature.js
@@ -1,12 +1,24 @@
+const { promisify } = require('util');
+const createHmacAsync = promisify(require('crypto').createHmac);
+
 const AppError = require('../../../../shared/errors/AppError');
 
 exports.validateSignature = async (req, res, next) => {
     const signature = req.body.headers['x-bold-signature'];
     const timestamp = req.body.headers['timestamp'];
 
+    const json_body = JSON.stringify(req.body);
+
+    const hash = createHmacAsync('sha256', '7cf3d39eaa3876396bfa78ac67e70c1a08337a9669305d08b7cb2821a2569cfae25b1118aa6b57e12d197e58f1bbac05c67abf2cace9d5851054b081172bb4a8')
+            .update(`${json_body}.${timestamp}`)
+            .digest();
+        
+    console.log(hash);
+    throw new AppError('Unauthorized.', 401);
+
     if (!signature || !timestamp) {
-        throw new AppError('Unauthorized.', 401);
+        
     }
 
-    next();
+    // next();
 }
--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const express = require("express");
 
 const subscriptionController = require('./api/subscriptions/infra/http/controllers/subscription');
 const recurringOrderController = require('./api/order/infra/http/controllers/recurringOrder');
+const { validateSignature } = require('./api/shared/infra/http/middlewares/validateSignature');
 
 const app = express();
 
 app.use(express.json({ extended: false }));
 
-app.post("/subscription", subscriptionController.handle);
+app.post("/subscription", validateSignature, subscriptionController.handle);
 app.post("/order", recurringOrderController.handle);
 
 const PORT = process.env.PORT || 3333;
